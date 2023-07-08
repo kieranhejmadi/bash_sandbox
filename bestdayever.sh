b60e65e72b2712876bd2fcbc2da0490bc5d51a0d
MINIT <kieran.hejmadi@nanoporetech.com>
2023-07-08 13:15:21 +0100

    Created an additional script

---
+++ bestdayever.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+user=$(whoami)
+name=$1
+compliment=$2
+echo "Good Morning $name, you're currently logged in as $user"
+echo "You're looking good today $name"
+echo "YOu have the best $compliment $name"
